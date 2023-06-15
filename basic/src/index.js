9dde84252c1e65fdfe7d8069d835f82c205feb2a
speranza0 <speranza0p@gmail.com>
2023-06-15 19:25:11 +0900

    study commit

--- basic/src/index.js
+++ basic/src/index.js
@@ -6,16 +6,37 @@ import reportWebVitals from "./reportWebVitals";
 
 import Library from "./chapter_03/Library";
 import Clock from "./chapter_04/Clock";
+import CommentList from "./chapter_05/CommentList";
+import NotificationList from "./chapter_06/NotificationList";
+import Accommodate from "./chapter_07/Accommodate";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-setInterval(() => {
-  root.render(
-    <React.StrictMode>
-      <Clock />
-    </React.StrictMode>
-  );
-}, 1000);
+// setInterval(() => {
+//   root.render(
+//     <React.StrictMode>
+//       <Clock />
+//     </React.StrictMode>
+//   );
+// }, 1000);
+
+// root.render(
+//   <React.StrictMode>
+//     <CommentList />
+//   </React.StrictMode>
+// );
+
+// root.render(
+//   <React.StrictMode>
+//     <NotificationList />
+//   </React.StrictMode>
+// );
+
+root.render(
+  <React.StrictMode>
+    <Accommodate />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
